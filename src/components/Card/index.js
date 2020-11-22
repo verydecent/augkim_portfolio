a8de2aea9c87ad111b195802e889c4f94cb26634
Wonjae Hwang <wnjhwng@gmail.com>
2020-11-22 15:33:49 -0800

    Create card bo ilerplate

---
+++ src/components/Card/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    console.log("=");
+  }
+
+  render() {
+    return <div className="card">Card</div>;
+  }
+}
