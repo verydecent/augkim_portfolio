5d7c64f1c59e43127f83313405d19597316557a1
Wonjae Hwang <wnjhwng@gmail.com>
2020-11-22 20:19:53 -0800

    Remove unused code

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,43 +1,15 @@
 import React from "react";
 import ProfilePic from "../ProfilePic";
-import client from "../../helpers/contentful";
 import Bio from "../Bio";
+import LinksList from "../LinksList";
 
 class Card extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pic: "",
-      name: "",
-      bio: "asdadasd",
-    };
-  }
-
-  componentDidMount() {
-    client
-      .getEntries(
-        "4aUkB4bzMjOI6BU04k2fSt",
-        "5VaxQBNFwlnNIaYzfhpiyM",
-        "5bNDKtltCEPzT7gzS6PwZF"
-      )
-      .then((res) => {
-        console.log("=========>", res);
-        this.setState({
-          pic: res.fields.profilePicture.fields.file.url,
-          name: res.fields.name,
-        });
-      })
-      .catch((err) => console.log(err));
-  }
-
   render() {
-    const { pic, name, bio } = this.state;
-
     return (
       <div className="card">
         <ProfilePic />
-        <Bio bio={bio} />
+        <Bio />
+        <LinksList />
       </div>
     );
   }
