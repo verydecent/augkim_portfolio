57251b7552a39b25c999685e0b86ddbfa32e4369
Wonjae Hwang <wnjhwng@gmail.com>
2020-11-22 20:19:21 -0800

    Remove

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import "./App.css";
-import { ContentfulClient, ContentfulProvider } from "react-contentful";
 import Routes from "./components/Routes";
-
-const client = new ContentfulClient({
-  space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
-});
 
 function App() {
   return (
     <div className="App">
-      <ContentfulProvider client={client}>
-        <Routes />
-      </ContentfulProvider>
+      <Routes />
     </div>
   );
 }
