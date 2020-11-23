a7684f3713e8b49de6bd1e73d8401a9b73461319
Wonjae Hwang <wnjhwng@gmail.com>
2020-11-22 20:20:33 -0800

    Remove console.logg

---
+++ src/components/LinksList/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import client from "../../helpers/contentful";
+import { Link } from "react-router-dom";
+
+class LinksList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { links: [] };
+  }
+
+  componentDidMount() {
+    client
+      .getEntries()
+      .then((res) => {
+        const newArr = [];
+        res.items.forEach((link) => {
+          if (link.sys.contentType.sys.id === "gallery") {
+            newArr.push(link.fields.title);
+          }
+        });
+        this.setState({ links: newArr });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    const links = this.state.links.map((link, index) => (
+      <Link key={index} to={link}>
+        {link}
+      </Link>
+    ));
+    return <div className="lists-list">{links}</div>;
+  }
+}
+
+export default LinksList;
